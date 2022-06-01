b7ee14a9bdf473d0b115a018d10a58bc0c0d3345
0amfibinator0 <00amfibiantor00@mail.ru>
2022-06-01 23:57:05 +0300

    первый урок

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 function App() {
-    return <div className = "wrapper" >
-        <header>
-            <div className="headerLeft">
-                <svg />
-                <div className="headerInfo">
-                    <h3 > React Sneakers </h3>
-                    <p> Магазин лучших кроссовок </p>
+    return <div className = "wrapper clear" >
+        <header className="d-flex justify-between align-center p-40">
+            <div className="d-flex align-center">
+                <img className="logo" src="/img/logo.png" alt="" />
+                <div>
+                    <h3 className="text-uppercase"> React Sneakers</h3>
+                    <p> Магазин лучших кроссовок</p>
                 </div>
             </div>
-            <ul className="headerRight">
-                <li>
-                    <svg />
+            <ul className="d-flex">
+                <li className="mr-30">
+                    <img width={18} height={18} src="/img/cart.svg" alt="" />
                     <span>1205 руб.</span>
                 </li>
                 <li>
-                    <svg />
+                    <img width={18} height={18} src="/img/user.svg" alt="" />
                 </li>
             </ul>
         </header>
         <div className="container">
-            
+            <h1>Все кроссовки</h1>
+            ....
         </div>
     </div>
 }
